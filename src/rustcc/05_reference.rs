ede55980982c92e2f0aeb043c506d2c4228dc871
difficcd <144074124+difficcd@users.noreply.github.com>
2025-09-08 22:16:59 +0900

    0908

--- src/rustcc/05_reference.rs
+++ src/rustcc/05_reference.rs
@@ -4,19 +4,40 @@ fn main () {
 
     println!("v: {}, r: {}", v, r) ;
 
-    /*
+    
     println!("v: {}, r: {:p}", v, r) ;
-    //let p : *const u32 = &v ;
-    */
+    let p : *const u32 = &v ;
+    
 
-    /*
+    
     let mut v : i32 = 2026 ;
     let p = &mut v ;
     *p = 2024 ;
     println!("p: {}", p) ;
-    */
-    /*
+   
     println!("v: {}", v) ;
-    */
+    
+
+}
+
+// memo
+fn main () {
+    let v : i32 = 2025 ;
+    let r = &v ;
+
+    println!("v: {}, r: {}", v, r) ;
+
+    println!("v: {}, r: {:p}", v, r) ;
+    let p : *const i32 = &v ; 
+    // unsig X (type 일치해야함)
+
+    let mut v : i32 = 2026 ;
+    let p = &mut v ;
+    *p = 2024 ;
+
+    println!("p: {}", p) ; // 2024
+
+    println!("v: {}", v) ; // 2024
+
 
 }

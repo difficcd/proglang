40996bc359effdaf540f3c8e4472cb91cc274156
difficcd <144074124+difficcd@users.noreply.github.com>
2025-10-14 00:26:22 +0900

    Update main.rs

--- src/vae/src/main.rs
+++ src/vae/src/main.rs
@@ -15,18 +15,22 @@ lalrpop_mod!(pub vae) ;
 //     let mut map = BTreeMap::new(); 이런 식으로 정의하고,
 //     map.insert("one".to_string(), 1); 이런 식으로 사용함
 
+// 이러한 BTreeMap의 key-value 구조로 identifier 를 binding occurrence!
+// String 은 값에 부여할 변수 이름이고, i32는 본래의 value
+
 fn interp (e: Box<Expr>, env: &BTreeMap::<String, i32>) -> i32 
-               
 {
     match *e {
         Op(l, Add, r) => interp(l, env) + interp(r, env),
         Op(l, Sub, r) => interp(l, env) - interp(r, env),
         Num(n) => n,
         Ref(id) => *env.get(&id).unwrap(),
+        // id 형태면 값을 가져오도록 env 에 역참조시킴
         Val(id, v, e) => {
+        // 새 변수 선언하고, nenv(env복사본:scope구분위해) 에 바인딩
             let mut nenv = env.clone() ;
             nenv.insert(id, interp(v, env)) ;
-            interp(e, &nenv)
+            interp(e, &nenv) // 재귀적으로 e를 다시 해석하도록 함
         }
     } 
 }
@@ -35,6 +39,8 @@ fn interp (e: Box<Expr>, env: &BTreeMap::<String, i32>) -> i32
 fn main() 
 {
     let env = BTreeMap::<String, i32>::new() ;
+    
+    // val 이라는 keyword 는 lalrpop 에 저장되어 있음!
     let e = vae::ExprParser::new().parse("val i=3 in (i + (1 + i))").unwrap() ;
     println!("e: {}", e) ;
     println!("interp(e,env): {}", interp(e, &env)) ;

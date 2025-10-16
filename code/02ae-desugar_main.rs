8e078472458fd568e941fa49fd16c7e7a37c0dde
difficcd <144074124+difficcd@users.noreply.github.com>
2025-10-16 09:24:59 +0900

    refine

--- code/02ae-desugar_main.rs
+++ code/02ae-desugar_main.rs
@@ -3,7 +3,7 @@ use lalrpop_util::lalrpop_mod ;
 pub mod ast ;
 use ast::Expr ;
 use ast::Expr::{Op, Num, Neg} ;
-use ast::Opcode::{Add, Sub} ;
+use ast::Opr::{Add, Sub} ;
 
 lalrpop_mod!(pub ae) ;
 
@@ -12,7 +12,7 @@ fn interp (e: Box<Expr>) -> i32
     match *e {
         Op(l, Add, r) => interp(l) + interp(r),
         Op(l, Sub, r) => interp(l) - interp(r),
-        Neg(exp) => -1 * interp(exp), // Semantics 정의
+        Neg(exp) => -1 * interp(exp), 
         Num(n) => n,
     } 
 }
@@ -23,7 +23,6 @@ fn desugar (e: Box<Expr>) -> Box<Expr>
         Op(l, Add, r) => Box::new(Op(desugar(l), Add, desugar(r))),
         Op(l, Sub, r) => Box::new(Op(desugar(l), Sub, desugar(r))),
         Neg(e) => Box::new(Op(Box::new(Num(0)), Sub, desugar(e))),
-                  //   "-" "(" <expr> ")"   to   "0“ - ”(" <expr> ")“
         Num(n) => Box::new(Num(n)), 
     }
 }
@@ -31,12 +30,16 @@ fn desugar (e: Box<Expr>) -> Box<Expr>
 
 fn main() 
 {   
+  
     let e0 = ae::ExprParser::new().parse("(-(5 - 1) + 3)").unwrap() ;
     println!("e0: {}", e0) ;
-    println!("interp(e0): {}", interp(e0.clone())) ;
+    println!("e0 AST: {:?}", e0) ;
+    println!("interp(e0): {}", interp(e0.clone())) ; 
     println!("") ;
 
     let e1 = desugar(e0.clone()) ;
     println!("e1=desugar(e0): {}", e1) ;
-    println!("interp(e1): {}", interp(e1)) ; 
+    println!("e1 AST: {:?}", e1) ; 
+    println!("interp(e1): {}", interp(e1)) ;
+  
 }

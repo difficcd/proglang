d80786d8bca160bc2d8c4fffcdb4360b89c8bfc7
difficcd <144074124+difficcd@users.noreply.github.com>
2025-10-19 15:33:31 +0900

    main 설명 주석 추가

--- src/vae/src/main.rs
+++ src/vae/src/main.rs
@@ -25,12 +25,14 @@ fn interp (e: Box<Expr>, env: &BTreeMap::<String, i32>) -> i32
         Op(l, Sub, r) => interp(l, env) - interp(r, env),
         Num(n) => n,
         Use(id) => *env.get(&id).unwrap(),
-        // id 형태면 값을 가져오도록 env 에 역참조시킴
+         // BTreeMap reference env 에서, value 를 가져오려면
+        // *env.get(&key) 로 가져오되 Result type 처리로 unwrap() 붙여줘야 함
         Val(id, v, e) => {
-        // 새 변수 선언하고, nenv(env복사본:scope구분위해) 에 바인딩
             let mut nenv = env.clone() ;
             nenv.insert(id, interp(v, env)) ;
+            println!("nenv inside Val: {:?}", nenv) ; // 확인용 출력 추가
             interp(e, &nenv) // 재귀적으로 e를 다시 해석하도록 함
+                        // block expression
         }
     } 
 }
@@ -38,6 +40,10 @@ fn interp (e: Box<Expr>, env: &BTreeMap::<String, i32>) -> i32
 
 fn main() 
 {
+    // Main에서의 테스트 코드도 작성할 줄 알아야 함
+    // BTreeMap 만드는 과정, parse 함수 작성 과정도 연습
+    // 파싱하는거랑, helper 없이 하는거랑, helper 로 하는거 세가지 모두 익히기
+    
     let env = BTreeMap::<String, i32>::new() ;
     let e0 = vae::ExprParser::new().parse("val i=3 in (i + (1 + i))").unwrap() ;
     println!("e0: {}", e0) ;
@@ -50,4 +56,12 @@ fn main()
     println!("e1: {:?}", e1) ;
             // AST: Val("i", Num(3), Op(Use("i"), Add, Val("i", Num(5), Op(Num(1), Add, Use("i")))))
     println!("interp(e1,[]): {}", interp(e1, &env)) ;
+
+
+        let e2 = vae::ExprParser::new().parse("val i=5 in (i + 10)").unwrap() ;
+    println!("e2: {}", e2) ;
+    println!("e2: {:?}", e2) ;
+    println!("interp(e2,[]): {}", interp(e2, &env)) ;
+    // 간단한 case 추가, AST : Val("i", Num(5), Op(Use("i"), Add, Num(10)))
+    
 }

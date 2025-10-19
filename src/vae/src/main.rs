8dd0ac71acac179af0429e1f545fc3ab24c7cc29
difficcd <144074124+difficcd@users.noreply.github.com>
2025-10-19 16:01:18 +0900

    VAE 이해 - 주석, test코드 추가

--- src/vae/src/main.rs
+++ src/vae/src/main.rs
@@ -20,6 +20,7 @@ lalrpop_mod!(pub vae) ;
 
 fn interp (e: Box<Expr>, env: &BTreeMap::<String, i32>) -> i32 
 {
+    // env 는 전제(σ)로, match *e시 variable의 evaluate에 직접관여(Use,Val)
     match *e {
         Op(l, Add, r) => interp(l, env) + interp(r, env),
         Op(l, Sub, r) => interp(l, env) - interp(r, env),
@@ -28,6 +29,7 @@ fn interp (e: Box<Expr>, env: &BTreeMap::<String, i32>) -> i32
          // BTreeMap reference env 에서, value 를 가져오려면
         // *env.get(&key) 로 가져오되 Result type 처리로 unwrap() 붙여줘야 함
         Val(id, v, e) => {
+            // Sematics: id-v 매핑후 expression 에 적용(전제) : evaluate
             let mut nenv = env.clone() ;
             nenv.insert(id, interp(v, env)) ;
             println!("nenv inside Val: {:?}", nenv) ; // 확인용 출력 추가
@@ -58,10 +60,26 @@ fn main()
     println!("interp(e1,[]): {}", interp(e1, &env)) ;
 
 
-        let e2 = vae::ExprParser::new().parse("val i=5 in (i + 10)").unwrap() ;
+    let e2 = vae::ExprParser::new().parse("val i=5 in (i + 10)").unwrap() ;
     println!("e2: {}", e2) ;
     println!("e2: {:?}", e2) ;
     println!("interp(e2,[]): {}", interp(e2, &env)) ;
-    // 간단한 case 추가, AST : Val("i", Num(5), Op(Use("i"), Add, Num(10)))
+
+     let e2 = vae::ExprParser::new().parse("val i=(0 + 5) in (i + 10)").unwrap() ;
+    println!("e2: {}", e2) ;
+    println!("e2: {:?}", e2) ;
+    println!("interp(e2,[]): {}", interp(e2, &env)) ;
+    // 간단한 case 추가, AST : Val("i", Op(Num(0), Add, Num(5)), Op(Use("i"), Add, Num(10)))
+    	// 왜 두 번째 요소도 &Box여야 하는가?
+	    //  i=(0+5) 같은 형식일 수 있기 때문! 
+
+    
+    // use helper function : val i=(1+10) in (i-1)
+    // 중요! 그냥 val ... 이런식으로 주면 &str 으로, 파서에 보낸다면 파서는 알아서 처리하지만
+    // helper 함수를 사용하는 경우, 그냥 ""로 적으면 String이 들어가야 할 자리에 &str이 들어가버린다.
+    let e3 = val(String::from("i"), add(num(1),num(10)), sub(useid(String::from("i")),num(1))) ; 
+    println!("e3: {}", e3) ;
+    println!("e3: {:?}", e3) ;
+    println!("interp(e3,[]): {}", interp(e3, &env)) ;
     
 }

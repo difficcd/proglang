76255cee7f8509450eb003af691c1704d35913be
difficcd <144074124+difficcd@users.noreply.github.com>
2025-11-04 15:02:41 +0900

    val -> let & Call Expr 추가

--- src/f1vae/ast.rs
+++ src/f1vae/ast.rs
@@ -5,11 +5,11 @@ pub enum Expr {
     Num(i32),
     Op(Box<Expr>, Opr, Box<Expr>),
 
-    Val(String, Box<Expr>, Box<Expr>),
+    Let(String, Box<Expr>, Box<Expr>),
     Use(String),
 
-	// Val("i", Num(5), Op(Use("i"), Add Num(10)))
-	//      >> val i=5 in (i + 10)
+	// Let("i", Num(5), Op(Use("i"), Add Num(10)))
+	//      >> let i=5 in (i + 10)
 
 	// Use 는 Num(3)으로 매핑된 변수 i를 사용한다는 뜻
 	// :Use(id) => *env.get(&id).unwrap()
@@ -38,8 +38,8 @@ pub fn num (n: i32) -> Box<Expr>
 
 
 
-pub fn val (id: String, v: Box<Expr>, e: Box<Expr>) -> Box<Expr>
-{ Box::new(Expr::Val(id, v, e)) }
+pub fn let_ (id: String, v: Box<Expr>, e: Box<Expr>) -> Box<Expr>
+{ Box::new(Expr::Let(id, v, e)) }
 
 pub fn useid (id: String) -> Box<Expr>
 { Box::new(Expr::Use(id)) }
@@ -52,7 +52,7 @@ impl fmt::Display for Expr
 		match self {
 			Expr::Num(n) => write!(f, "{}", n),
 			Expr::Op(l, op, r) => write!(f, "({} {} {})", l, op, r),
-			Expr::Val(x, id, e) => write!(f, "val {}={} in {}", x, id, e),
+			Expr::Let(x, id, e) => write!(f, "let {}={} in {}", x, id, e),
 			Expr::Use(id) => write!(f, "{}", id),
 			Expr::Call(fname, arg) => write!(f, "{}({})", fname, arg), 
 			  // Call에 대한 Display trait 구현
